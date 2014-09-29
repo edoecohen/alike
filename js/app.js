92fd9c71b0d20f2b34adca4c06c6293a6cf0cdcd
Edoe Cohen <edoecohen@gmail.com>
2014-09-29 10:29:00 -0700

    Added Alike button and more...
    - Changed Book recommendations to generic terminology
    - Show the query item as well in the results

--- js/app.js
+++ js/app.js
@@ -4,12 +4,13 @@ $(document).ready( function() {
 	// USER SUBMITS A QUERY
 	$('.recommendation-getter').submit( function(event){
 		// zero out results if previous search has run
-		$('.results').html('');
+		$('.results').empty();
 		// get the value of the tags the user submitted
 		var query = $(this).find("input[name='query']").val();
 		getRecommendations(query);
 	});
 
+	var allRecs = new Array();
 
 	// GET BOOK RECOMMENDATIONS
 	var getRecommendations = function(query) {
@@ -19,26 +20,31 @@ $(document).ready( function() {
 			f: "gifted2903",
 			k: "nwfhotk2owzk",
 			format: "JSON",
-			jsonp: "handleToReturn",
 			verbose: 1
 		};
 		
 		$.ajax({
-			url: "http://www.tastekid.com/ask/ws?q="+query,
+			url: "http://www.tastekid.com/ask/ws?q="+query+"//books&jsonp=bookRecs",
 			data: request,
 			contentType: "application/json",
 			dataType: "jsonp",
 			type: "GET",
-		})
+		})		
+		.done(bookRecs = function (myData) {
 
-		//handleToReturn = function (myData) {
-		//	console.log(myData);
-		//	console.log(myData.Similar.Results);
-		//}
-		
-		.done(handleToReturn = function (myData) {
+			console.log(myData);
+			allRecs.push(myData);
+			console.log(allRecs);
+
+			// SHOW THE QUERY RESULTS FIRST
+			$.each(myData.Similar.Info, function(i, item) {
+				var showResult = showRec(item);
+				$('.results').prepend(showResult);
+			});
+
+			//SHOW THE RECOMMENDATION RESULTS
 			$.each(myData.Similar.Results, function(i, item) {
-				var book = showBook(item);
+				var book = showRec(item);
 				$('.results').append(book);
 			});
 		});
@@ -46,22 +52,36 @@ $(document).ready( function() {
 		//	var errorElem = showError(error);
 		//	$('.search-results').append(errorElem);
 		//});
+
+
 	};
 
-// SHOW RESULTS FUNCTION
-	var showBook = function(book) {
+// SHOW RECOMMENDATIONS FUNCTION
+	var showRec = function(recommendation) {
 		
 		// CLONE OUR RESULT TEMPLATE CODE
-		var result = $('.templates .bookResult').clone();
+		var result = $('.templates .recResult').clone();
 
 		// SET THE TITLE FOR THE BOOK
-		var bookTitle = result.find('.bookTitle a');
-		bookTitle.text(book.Name);
-		bookTitle.attr('href', book.wUrl);
+		var bookTitle = result.find('.recTitle a');
+		bookTitle.text(recommendation.Name);
+		bookTitle.attr('href', recommendation.wUrl);
+
+		// SET THE TITLE OF THE BOOK
+		var recType = result.find('.recType');
+		recType.text(recommendation.Type);
 
 		// SET THE DESCRIPTION OF THE BOOK
-		var bookDescription = result.find('.bookDescription');
-		bookDescription.text(book.wTeaser);
+		var bookDescription = result.find('.recDescription');
+		bookDescription.text(recommendation.wTeaser);
+
+		// When user clicks 'Alike' take the recTitle and pass into query
+		var alikeButton = result.find('.alikeButton');
+		alikeButton.on('click', function() {
+			var query = recommendation.Name;
+			$('.results').empty();
+			getRecommendations(query);
+		});
 
 		return result;
 	};
