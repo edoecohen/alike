b32ea8fc455c389a3595248220b8ed9fcb03cdad
Edoe Cohen <edoecohen@gmail.com>
2014-09-29 08:41:52 -0700

    Got the TasteKid API to work!!!
    + changed app name from Ex Libris to Alike

--- js/app.js
+++ js/app.js
@@ -1,53 +1,70 @@
 $(document).ready( function() {
 	
-	
+		
+	// USER SUBMITS A QUERY
+	$('.recommendation-getter').submit( function(event){
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var query = $(this).find("input[name='query']").val();
+		getRecommendations(query);
+	});
+
+
+	// GET BOOK RECOMMENDATIONS
 	var getRecommendations = function(query) {
 		
 		// the parameters we need to pass in our request to TasteKid's API
 		var request = {
-			q: query,
 			f: "gifted2903",
 			k: "nwfhotk2owzk",
 			format: "JSON",
 			jsonp: "handleToReturn",
-
+			verbose: 1
 		};
 		
 		$.ajax({
-			url: "http://www.tastekid.com/ask/ws",
+			url: "http://www.tastekid.com/ask/ws?q="+query,
 			data: request,
 			contentType: "application/json",
 			dataType: "jsonp",
 			type: "GET",
-		});
+		})
 
-
-		handleToReturn = function (myData) {
-			console.log(myData);
-			console.log(myData.Similar.Results);
-		};
-		//.done(function(result) {
+		//handleToReturn = function (myData) {
+		//	console.log(myData);
+		//	console.log(myData.Similar.Results);
+		//}
 		
-			//$.each(result.results, function(i, item) {
-			//	var question = showBooks(item);
-			//	$('.results').append(question);
-			//});
-		//});
+		.done(handleToReturn = function (myData) {
+			$.each(myData.Similar.Results, function(i, item) {
+				var book = showBook(item);
+				$('.results').append(book);
+			});
+		});
 		//.fail(function(jqXHR, error, errorThrown){
 		//	var errorElem = showError(error);
 		//	$('.search-results').append(errorElem);
 		//});
 	};
 
-	
-	// USER SUBMITS A QUERY
-	$('.recommendation-getter').submit( function(event){
-		// zero out results if previous search has run
-		// $('.results').html('');
-		// get the value of the tags the user submitted
-		var query = $(this).find("input[name='query']").val();
-		getRecommendations(query);
-		console.log(query);
-	});
+// SHOW RESULTS FUNCTION
+	var showBook = function(book) {
+		
+		// CLONE OUR RESULT TEMPLATE CODE
+		var result = $('.templates .bookResult').clone();
+
+		// SET THE TITLE FOR THE BOOK
+		var bookTitle = result.find('.bookTitle a');
+		bookTitle.text(book.Name);
+		bookTitle.attr('href', book.wUrl);
+
+		// SET THE DESCRIPTION OF THE BOOK
+		var bookDescription = result.find('.bookDescription');
+		bookDescription.text(book.wTeaser);
+
+		return result;
+	};
+
 
 });
