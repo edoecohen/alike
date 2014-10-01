9af6d366894f8ef9edeba56851b945bfb4ca9af4
Edoe Cohen <edoecohen@gmail.com>
2014-10-01 02:05:08 -0700

    Got Bing image search API to work!
    But having trouble pushing all AJAX calls into one array and printing
    out the results.
    
    Will try to print them out individually.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
 $(document).ready( function() {
-	
-		
+
+var allRecs = [];	
+
 	// USER SUBMITS A QUERY
 	$('.recommendation-getter').submit( function(event){
 		// zero out results if previous search has run
-		$('.results').empty();
+
 		// get the value of the tags the user submitted
 		var query = $(this).find("input[name='query']").val();
 		getRecommendations(query);
-		getAmazonItemInfo(query);
 	});
 
-	var allRecs = [];
+	var music = "//music";
 	var books = "//books";
 	var authors = "//authors";
+	var movies = "//movies";
+	var shows = "//shows";
 
 	// GET BOOK RECOMMENDATIONS
 	var getRecommendations = function(query) {
-		
+		$('.results').empty();
+		allRecs.length = 0
+
 		// the parameters we need to pass in our request to TasteKid's API
 		var request = {
 			f: "gifted2903",
@@ -25,6 +29,18 @@ $(document).ready( function() {
 			format: "JSON",
 			verbose: 1
 		};
+
+		// GET MUSIC RECOMMENDATIONS
+		$.ajax({
+			url: "http://www.tastekid.com/ask/ws?q="+query+music+"&jsonp=musicRecs",
+			data: request,
+			contentType: "application/json",
+			dataType: "jsonp",
+			type: "GET"
+		})	
+		.success(musicRecs = function (myData) {
+			allRecs.push(myData.Similar.Results);
+		});
 
 		// GET BOOK RECOMMENDATIONS
 		$.ajax({
@@ -34,7 +50,31 @@ $(document).ready( function() {
 			dataType: "jsonp",
 			type: "GET"
 		})	
-		.done(bookRecs = function (myData) {
+		.success(bookRecs = function (myData) {
+			allRecs.push(myData.Similar.Results);
+		});
+
+		// GET MOVIE RECOMMENDATIONS
+		$.ajax({
+			url: "http://www.tastekid.com/ask/ws?q="+query+movies+"&jsonp=movieRecs",
+			data: request,
+			contentType: "application/json",
+			dataType: "jsonp",
+			type: "GET"
+		})	
+		.success(movieRecs = function (myData) {
+			allRecs.push(myData.Similar.Results);
+		});
+
+		// GET TV RECOMMENDATIONS
+		$.ajax({
+			url: "http://www.tastekid.com/ask/ws?q="+query+shows+"&jsonp=showRecs",
+			data: request,
+			contentType: "application/json",
+			dataType: "jsonp",
+			type: "GET"
+		})	
+		.success(showRecs = function (myData) {
 
 			// SHOW THE QUERY RESULTS FIRST
 			$.each(myData.Similar.Info, function(i, item) {
@@ -42,30 +82,19 @@ $(document).ready( function() {
 				$('.results').prepend(showResult);
 			});
 
-			// PUSH ALL BOOK RECS INTO allRecs ARRAY
-			$.each(myData.Similar.Results, function(i, item) {
-		        allRecs.push({
-		            Name: item.Name, 
-		            wUrl: item.wUrl,
-		            Type: item.Type,
-		            wTeaser: item.wTeaser
-		        });
-			});
+			allRecs.push(myData.Similar.Results);
+			console.log(allRecs);
 			
-			//SHOW BOOK RECOMMENDATIONS IN THE RESULTS
-			$.each(allRecs, function(i, item) {
-				var resultItem = showRec(item);
-				$('.results').append(resultItem);
-			});
-
-		//.fail(function(jqXHR, error, errorThrown){
-		//	var errorElem = showError(error);
-		//	$('.search-results').append(errorElem);
-		//});
+			//SHOW ALL RECOMMENDATIONS IN THE RESULTS
+			for (index = 0; index < allRecs.length; ++index) {
+			    $.each(allRecs[index], function(i, item) {
+					var resultItem = showRec(item);
+					$('.results').append(resultItem);
+				});
+			};	
 		});
 
 		console.log(allRecs);
-		
 	};
 
 // SHOW RECOMMENDATIONS FUNCTION
@@ -73,6 +102,51 @@ $(document).ready( function() {
 		
 		// CLONE OUR RESULT TEMPLATE CODE
 		var result = $('.templates .recResult').clone();
+
+		// PLACE THE IMAGE
+		var placeImage = function () {
+			
+			var imageURL;
+
+			// SEARCH FOR THE QUERY IMAGE VIA BING
+			function bingSearch(query) {            
+			//Build a new API uri.
+				var bingApiAppId = ":B5fcSyue4p3NPOJywYMxTf2ZkpAxGE2i75rJs2BPbgw";
+				var encodedAppKey = btoa(bingApiAppId);
+				var bingUri = BuildBingApiUri(query);
+
+		  		//Make the API call.        
+				var bingResult = $.ajax({
+				url: bingUri,
+				type: 'GET',
+				headers: {	"Authorization": "Basic " + encodedAppKey}
+				})
+				.success( bingResult = function (myData){
+					imageURL = myData.d.results[0].MediaUrl;
+					var recImage = result.find('.recImage img');
+					recImage.attr('src', imageURL);
+					console.log(imageURL);
+				});
+			};
+			
+			function BuildBingApiUri(query) {
+		 		//Build an uri for the Bing API call.                                
+				var bingApiUrl = "https://api.datamarket.azure.com/Bing/Search/v1/Image";
+				// var bingApiImageCount = "1";                
+				 
+				var s = bingApiUrl +
+				"?Query=%27" + query + "%27" +
+				//"&image.count=" + bingApiImageCount +
+				//"&Image.Offset=" + 0 +
+				"&$format=json";
+				 
+				return s;                
+			};    
+
+			bingSearch(recommendation.Name);
+
+		}; 
+		placeImage();
 
 		// SET THE TITLE FOR THE BOOK
 		var bookTitle = result.find('.recTitle a');
@@ -91,19 +165,14 @@ $(document).ready( function() {
 		var alikeButton = result.find('.alikeButton');
 		alikeButton.on('click', function() {
 			var query = recommendation.Name;
-			$('.results').empty();
 			getRecommendations(query);
 		});
 
 		return result;
 	};
 
-	
-	
-	//getAmazonItemInfo("catcher in the rye");
-	//var catcher = getAmazonItemInfo("catcher in the rye");
-	//var catcherParsed = xml2json.parser(catcher);
 
+           
 
 });
 
